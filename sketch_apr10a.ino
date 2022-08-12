fc162a148f5866022ac3dfedcac6f81646d338de
panocelot <69819938+panocelot@users.noreply.github.com>
2022-08-12 18:53:45 +0200

    Add files via upload

--- sketch_apr10a.ino
+++ sketch_apr10a.ino
@@ -1,9 +1,9 @@
 int a(0);
 int lastbuttonstate(0); //#zero is not pressed and one is pressed.
 
-void check(int x){
+void check(int x){ //checks if the number is even or odd
     if(x%2!=0){
-      digitalWrite(3, HIGH);
+      digitalWrite(3, HIGH); //led diode
       Serial.println(x);
   }else{
     digitalWrite(3, LOW);
@@ -13,12 +13,12 @@ void check(int x){
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, OUTPUT);
+  pinMode(2, INPUT_PULLUP); //a button
+  pinMode(3, OUTPUT); //a led diode
 }
  
 void loop() {
-  if(lastbuttonstate==0){
+  if(lastbuttonstate==0){ //makes sure that the button is not pressed.
     if(digitalRead(2)==LOW){
       a++;
       check(a);
@@ -28,7 +28,7 @@ void loop() {
     }
   }
 
-  if(lastbuttonstate==1){
+  if(lastbuttonstate==1){  //check if the button IS presed
     if(digitalRead(2)==LOW){
       check(a);
     }else{
